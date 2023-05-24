c4dc081e347f105871cd51516b91f4e583ab7d75
mbunix <bellianotieno24@gmail.com>
2023-05-24 14:36:20 +0300

    bellian statistics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,74 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Statistics from './components/statistics/statistics'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      {
-         Features
-         Pricing
-         Resources
-       
-         Login
-         Sign Up
-       
-         More than just shorter links
-       
-         Build your brand’s recognition and get detailed insights 
-         on how your links are performing.
-       
-         Get Started
-         
-         Shorten a link here...
-       
-         Shorten It!
-       
-         Advanced Statistics
-       
-         Track how your links are performing across the web with our 
-         advanced statistics dashboard.
-       
-         Brand Recognition
-       
-         Boost your brand recognition with each click. Generic links don’t 
-         mean a thing. Branded links help instil confidence in your content.
-       
-         Detailed Records
-       
-         Gain insights into who is clicking your links. Knowing when and where 
-         people engage with your content helps inform better decisions.
-       
-         Fully Customizable
-       
-         Improve brand awareness and content discoverability through customizable 
-         links, supercharging audience engagement.
-       
-         Boost your links today
-       
-         Get Started
-       
-         Features
-       
-         Link Shortening
-         Branded Links
-         Analytics
-       
-         Resources
-       
-         Blog
-         Developers
-         Support
-       
-         Company
-       
-         About
-         Our Team
-         Careers
-         Contact
-      }
+      <div className="App">
+       < Statistics />
+      </div>
+      
     </>
   )
 }
