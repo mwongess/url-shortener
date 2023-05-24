c4f44e2696ad7479d0d0b2036597b4bfbda48573
mwongess <amosmwongelah@gmail.com>
2023-05-24 13:12:06 +0300

    Lift footer to components

---
+++ client/src/components/footer/Cta.jsx
@@ -0,0 +1,12 @@
+import './cta.css'
+
+function Cta() {
+  return (
+    <div className="cta">
+        <h2 className="cta-heading">Boots your links today</h2>
+        <button className="cta-btn">Get Started</button>
+    </div>
+  )
+}
+
+export default Cta
