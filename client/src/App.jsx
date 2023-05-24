991dd18e1dc806f9274dceea708c9093928b8b30
mbunix <bellianotieno24@gmail.com>
2023-05-24 22:43:22 +0300

    validity of links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,41 @@
-import Statistics from "./components/statistics/statistics";
-import { createContext, useContext, useEffect, useState } from "react";
-import Footer from "./components/footer/Footer";
-import Header from "./components/header/header";
-import { Main } from "./components/main/Main";
-import Cta from "./components/cta/Cta";
-import "./App.css";
+import Statistics from './components/statistics/statistics'
+import { createContext, useContext, useEffect, useState } from 'react'
+import Footer from './components/footer/Footer'
+import Header from './components/header/header'
+import { Main } from './components/main/Main'
+import Cta from './components/cta/Cta'
+import './App.css'
 
-export const UrlShortenerContext = createContext();
+export const UrlShortenerContext = createContext()
 // custom hook
-export const useUrlShortenerContext = () => useContext(UrlShortenerContext);
+export const useUrlShortenerContext = () => useContext(UrlShortenerContext)
 
 const UrlShortenerProvider = ({ children }) => {
-  const [shortenedLinks, setShortenedLinks] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [buttonText, setButtonText] = useState("Copy");
-  const [shortUrl, setShortUrl] = useState("");
-  const [inputUrl, setInputUrl] = useState("")
-  const [url, setUrl] = useState("");
+  const [shortenedLinks, setShortenedLinks] = useState([])
+  const [errorMessage, setErrorMessage] = useState('')
+  const [buttonText, setButtonText] = useState('Copy')
+  const [shortUrl, setShortUrl] = useState('')
+  const [inputUrl, setInputUrl] = useState('')
+  const [url, setUrl] = useState('')
 
   // Fetch all previously shortened links
   useEffect(() => {
-    const storedLinks = localStorage.getItem("links");
+    const storedLinks = localStorage.getItem('links')
     if (storedLinks) {
-      setShortenedLinks(JSON.parse(storedLinks));
+      setShortenedLinks(JSON.parse(storedLinks))
     }
-  }, []);
+  }, [])
 
   // Update shortened links whenever they change
   useEffect(() => {
-    localStorage.setItem("links", JSON.stringify(shortenedLinks));
-  }, [shortenedLinks]);
+    localStorage.setItem('links', JSON.stringify(shortenedLinks))
+  }, [shortenedLinks])
 
   // Add new shortened link
   const addNewLink = (long_link, short_link) => {
-    const newLink = { long_link, short_link };
-    setShortenedLinks([newLink]);
-  };
+    const newLink = { long_link, short_link }
+    setShortenedLinks([newLink])
+  }
 
   // Shorten links and save them
   useEffect(() => {
@@ -45,54 +45,69 @@ const UrlShortenerProvider = ({ children }) => {
           const res = await fetch(
             `https://api.shrtco.de/v2/shorten?url=${url}`,
             {
-              method: "POST",
+              method: 'POST'
             }
-          );
-          const data = await res.json();
-          setShortUrl(data.result.short_link2);
+          )
+          const data = await res.json()
+          setShortUrl(data.result.short_link2)
         }
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
-    };
-    shortenUrl();
-  }, [url]);
+    }
+    shortenUrl()
+  }, [url])
 
-//Runs when there is a new short link 
+  //Runs when there is a new short link
   useEffect(() => {
     if (shortUrl) {
-      addNewLink(url, shortUrl);
+      addNewLink(url, shortUrl)
     }
-  },
-    [url, shortUrl])
+  }, [url, shortUrl])
 
   // React to changes on the inputs
-  const handleInputChange = (e) => {
-    setInputUrl(e.target.value);
-  };
+  const handleInputChange = e => {
+    setInputUrl(e.target.value)
+  }
 
   // React to button when it is clicked!
   const handleClick = () => {
-    if(inputUrl){
-      setUrl(inputUrl)
+    if (inputUrl) {
+      setUrl(inputUrl )
       setErrorMessage('Please add a link')
-    }else{
+    } else {
       alert('Please add a link')
     }
     setInputUrl('')
-  };
+  }
+  // check if the input url is valid
+  const isValid = () => {
+    if (inputUrl) {
+      // regex to check the validty of the url
+      const urlpattern = /^(ftp|http|https):\/\/[^ "]+$/
+      const isValidurl = urlpattern.test(inputUrl)
+      if (inputUrl && isValidurl) {
+        return true
+      } else {
+        setErrorMessage('Please add a valid url')
+        return false
+      }
+    } else {
+      return false
+    }
+  }
 
   //Copy shortened link to clipboard
-  const copyToClipboard = (text) => {
+  const copyToClipboard = text => {
     navigator.clipboard
       .writeText(text)
-      .then(() => setButtonText("Copied"))
-      .catch((error) => console.error("Error copying to clipboard:", error));
+      .then(() => setButtonText('Copied'))
+      .catch(error => console.error('Error copying to clipboard:', error))
 
     setTimeout(() => {
-      setButtonText("Copy");
-    }, 3000);
-  };
+      setButtonText('Copy')
+    }, 3000)
+  }
 
   return (
     <UrlShortenerContext.Provider
@@ -105,17 +120,18 @@ const UrlShortenerProvider = ({ children }) => {
         shortUrl,
         setShortUrl,
         buttonText,
+        isValid,
         copyToClipboard,
         shortenedLinks,
-        errorMessage,
+        errorMessage
       }}
     >
       {children}
     </UrlShortenerContext.Provider>
-  );
-};
+  )
+}
 
-function App() {
+function App () {
   return (
     <>
       <UrlShortenerProvider>
@@ -126,7 +142,7 @@ function App() {
         <Footer />
       </UrlShortenerProvider>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/components/main/Main.jsx
+++ client/src/components/main/Main.jsx
@@ -1,43 +1,71 @@
 import { useUrlShortenerContext } from '../../App'
 import './main.css'
 
-
 export const Main = () => {
-    const {inputUrl,shortenedLinks,url, handleClick, handleInputChange, shortUrl, buttonText, copyToClipboard, errorMessage } = useUrlShortenerContext()
-    return (
-        <>
-            <div className="main">
-                <div className="main-left">
-                    <h1>More than just shorter links</h1>
-                    <p>Build your brand’s recognition and get detailed insights
-                        on how your links are performing.</p>
-                    <button className='get-started'>Get Started</button>
+  const {
+    inputUrl,
+    shortenedLinks,
+    url,
+    handleClick,
+    handleInputChange,
+    shortUrl,
+    buttonText,
+    copyToClipboard,
+    errorMessage,
+    isValid
+  } = useUrlShortenerContext()
+  return (
+    <>
+      <div className='main'>
+        <div className='main-left'>
+          <h1>More than just shorter links</h1>
+          <p>
+            Build your brand’s recognition and get detailed insights on how your
+            links are performing.
+          </p>
+          <button className='get-started'>Get Started</button>
+        </div>
+        <div className='main-right'>
+          <img src='../images/illustration-working.svg' alt='' />
+        </div>
+      </div>
+      <div className='shorten-container'>
+        <div className='shorten'>
+          <input
+            type='text'
+            placeholder='Shorten a link here ...'
+            onChange={handleInputChange}
+            value={inputUrl}
+            className={!isValid() ? 'invalid' : ''}
+          />
+          <button onClick={handleClick}>Shorten It!!</button>
+          {!isValid() && <p className='error-message'>{errorMessage}</p>}
+        </div>
+        <div className='shortened-links'>
+          {shortUrl &&
+            shortenedLinks.map((shortened_Link, index) => (
+              <div className='short-link' key={index}>
+                <p>https://{shortened_Link.long_link}</p>
+                <div className='short-link-right'>
+                  <p>
+                    <a
+                      href='https://{shortened_Link.short_link}'
+                      target='_blank'
+                    >
+                      https://{shortened_Link.short_link}
+                    </a>
+                  </p>
+                  <button
+                    onClick={() => copyToClipboard(shortUrl)}
+                    className={buttonText}
+                  >
+                    {buttonText}
+                  </button>
                 </div>
-                <div className="main-right">
-                    <img src="../images/illustration-working.svg" alt="" />
-                </div>
-            </div>
-            <div className='shorten-container'>
-                <div className='shorten'>
-                    <input  type="text" placeholder='Shorten a link here ...' onChange={handleInputChange} value={inputUrl} />
-                    <button onClick={handleClick}>Shorten It!!</button>
-                </div>
-                <div className="shortened-links">
-                    {
-                       shortUrl && shortenedLinks.map((shortened_Link, index)=>(
-                            <div className="short-link" key={index}>
-                            <p>https://{shortened_Link.long_link}</p>
-                            <div className="short-link-right">
-                                <p><a href='https://{shortened_Link.short_link}' target='_blank'>https://{shortened_Link.short_link}</a></p>
-                                <button onClick={() => copyToClipboard(shortUrl)} className={buttonText}>{buttonText}</button>
-                            </div>
-                        </div>
-                        ))
-                    }
-
-                </div>
-
-            </div>
-        </>
-    )
+              </div>
+            ))}
+        </div>
+      </div>
+    </>
+  )
 }
